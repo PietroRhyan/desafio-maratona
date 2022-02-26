6c096e51ccccb4a5c65407752c04cb3b461aa908
Pietro Rhyan <pietroqjg@gmail.com>
2022-02-26 00:43:41 -0300

    Upgrading the responsivity

---
+++ script.js
@@ -0,0 +1,10 @@
+// Funcionalidades do Modal
+
+const Modal = {
+  open(){
+    document.querySelector('.modal-overlay').classList.add('active')
+  },
+  close(){
+    document.querySelector('.modal-overlay').classList.remove('active')
+  }
+}
