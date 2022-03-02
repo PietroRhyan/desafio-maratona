7acbf0986f1fdd42d8baa595424313352cbc11a6
Pietro Rhyan <pietroqjg@gmail.com>
2022-03-01 21:43:40 -0300

    Fixing some bugs

--- script.js
+++ script.js
@@ -63,9 +63,9 @@ const Transactions = {
 
 const Utils = {
   formatAmount(value) {
-    value = Number(value.replace(/\,\./g, '')) * 100
+    value = value * 100
 
-    return value
+    return Math.round(value)
   },
 
   formatDate(date) {
